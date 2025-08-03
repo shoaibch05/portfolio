586107a10167099ca56c75b6c2f785420ba48ca1
shoaibch05 <shoaibshamrez@gmail.com>
2025-08-03 15:58:30 +0500

    updated

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -35,7 +35,7 @@ const Hero = () => {
             initial={{ opacity: 0, x: 30 }}
             whileInView={{ opacity: 1, x: 0 }}
             transition={{ duration: 1, delay: 0.6 }}
-            className="text-gray-400"
+            className="text-green-600"
           >
             IT Graduate | Jr Web Developer | Jr Software Engineer
           </motion.p>
