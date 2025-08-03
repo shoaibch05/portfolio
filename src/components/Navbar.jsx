3c98e94881a525959ee9599e3b2bde1d44b3851d
shoaibch05 <shoaibshamrez@gmail.com>
2025-08-03 16:07:33 +0500

    Updated Resume

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,14 +13,21 @@ const Navbar = () => {
   return (
     <nav className="fixed w-full top-0 z-50 text-white flex justify-between items-center px-10 py-4 backdrop-blur-lg shadow-lg">
       <h1 className="text-2xl font-bold">&lt;Shoaib Shamrez/&gt;</h1>
-      
+
       {/* Mobile Menu Button */}
-      <button className="md:hidden text-white text-2xl" onClick={() => setMenuOpen(!menuOpen)}>
+      <button
+        className="md:hidden text-white text-2xl"
+        onClick={() => setMenuOpen(!menuOpen)}
+      >
         {menuOpen ? <FaTimes /> : <FaBars />}
       </button>
-      
+
       {/* Navbar Items */}
-      <ul className={`md:flex gap-6 items-center absolute md:static top-16 left-0 w-full bg-black md:bg-transparent md:w-auto flex flex-col md:flex-row transition-transform duration-300 ease-in-out ${menuOpen ? "block" : "hidden"}`}>
+      <ul
+        className={`md:flex gap-6 items-center absolute md:static top-16 left-0 w-full bg-black md:bg-transparent md:w-auto flex flex-col md:flex-row transition-transform duration-300 ease-in-out ${
+          menuOpen ? "block" : "hidden"
+        }`}
+      >
         {["home", "about", "experience", "contact"].map((section) => (
           <li key={section} className="text-center py-2 md:py-0">
             <a
@@ -33,7 +40,7 @@ const Navbar = () => {
           </li>
         ))}
         <a
-          href="/Shoaib_Shamrez_CV.pdf"
+          href="/Shoaib_Shamrez_CV_WebDev.pdf"
           target="_blank"
           className="bg-green-500 px-4 py-2 rounded-md hover:bg-green-600 transition text-center md:ml-4"
         >
